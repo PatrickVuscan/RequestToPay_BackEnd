df278674547d9ffda033e90677bb477b2f9def9b
Will Gerecke <wlgfour@gmail.com>
2019-10-29 13:53:41 -0400

    changed npm start and port declaration in server.ts

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ applyMiddleware(middleware, router);
 applyRoutes(routes, router);
 applyMiddleware(errorHandlers, router);
 
-const { PORT = 3000 } = process.env;
+const PORT = process.env.PORT || 3000;
 const server = http.createServer(router);
 
 server.listen(PORT, () => {
