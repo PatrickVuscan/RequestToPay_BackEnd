e25dc509e07d5c9b4a705ea00fd6c7806b91f2d3
Will Gerecke <wlgfour@gmail.com>
2019-12-05 12:37:28 -0500

    last db changes

--- RequestToPayData.sql
+++ RequestToPayData.sql
@@ -5,18 +5,20 @@ set search_path to "RequestToPay";
 
 -- Entity
 insert into "Entity"("EID", "Name", "BillingAddress", "Username", "Password", "PhoneNumber")
-values (default, 'Coke',         '20 Everywhere Ave.', 'coke',      'coke',      '6138578931'),
-       (default, 'McDonalds',    '-1 College s.t.',    'mcdonalds', 'mcdonalds', '7789906284'),
-       (default, 'Chestnut Res', '89 Chestnut s.t.',   'chesnut',   'chesnut',   null),
-       (default, 'Patrick',      '66 Broadway Ave.',   'patrick',   'patrick',   '6475158060'),
-       (default, 'Driver Mac',   '33 Orchard Blvd.',   'driver',    'zoomzoom',  '6478602203'),
-       (default, 'Will G',       'Chestnut Res.',      'will',      'will',      '9786185596');
+values (default, 'The North Pole', '20 Everywhere Ave.', 'santa123',  'santa123',  '6138578931'),
+       (default, 'McDonalds',      '-1 College s.t.',    'mcdonalds', 'mcdonalds', '7789906284'),
+       (default, 'Chestnut Res',   '89 Chestnut s.t.',   'chesnut',   'chesnut',   null),
+       (default, 'Patrick',        '66 Broadway Ave.',   'patrick',   'patrick',   '6475158060'),
+       (default, 'Driver Mac',     '33 Orchard Blvd.',   'driver',    'zoomzoom',  '6478602203'),
+       (default, 'Will G',         'Chestnut Res.',      'will',      'will',      '9786185596');
 
 -- Items
 insert into "Item"("IID", "SID", "Name", "Price")
 values (default, 1, 'Pallet of Diet Coke', 99.99),
        (default, 1, 'Pallet of Coke',      105.49),
-       (default, 1, 'Pallet of Fanta',     106.99);
+       (default, 1, 'Pallet of Fanta',     106.99),
+       (default, 1, 'Candy Candes',        150.99),
+       (default, 1, 'Hot Chocolate',       128.99);
 
 -- Invoice
 insert into "Invoice"("InID", "NextInID", "DeliveryDate")
@@ -38,7 +40,10 @@ values (default, null, '2019-01-02'),
 -- InvoiceItems
 insert into "InvoiceItems"("InID", "IID", "Price", "Quantity")
 values (1, 1, 99.98,  3),
-       (1, 2, 105.49, 2);
+       (1, 2, 105.49, 2),
+       (1, 3,   5.01, 500),
+       (1, 4, 105.49, 2),
+       (1, 5, 105.49, 2);
 
 -- WarehouseContents
 insert into "WarehouseContents"("SID", "IID", "Quantity", "Location")
@@ -53,8 +58,8 @@ values (default, 1, 3, 1,  5, '2019-01-01', true,  true,  true,  true ),
        (default, 1, 4, 7,  5, '2019-09-22', true,  true,  false, false),
        (default, 1, 4, 8,  5, '2019-09-25', true,  true,  false, false),
        (default, 1, 4, 9,  5, '2019-10-11', false, false, false, false),
-       (default, 1, 4, 10, 5, '2019-10-30', false, false, false, false),
-       (default, 1, 4, 11, 5, '2019-11-5',  true,  false, false, false),
-       (default, 1, 4, 12, 5, '2019-11-6',  true,  false, false, false),
-       (default, 1, 4, 13, 5, '2019-11-11', true,  true,  true,  true ),
-       (default, 1, 4, 14, 5, '2019-11-20', true,  false, false, false);
+       (default, 1, 4, 10, 6, '2019-10-30', false, false, false, false),
+       (default, 1, 4, 11, 6, '2019-11-5',  true,  false, false, false),
+       (default, 1, 4, 12, 6, '2019-11-6',  true,  false, false, false),
+       (default, 1, 4, 13, 6, '2019-11-11', true,  true,  true,  true ),
+       (default, 1, 4, 14, 6, '2019-11-20', true,  false, false, false);
